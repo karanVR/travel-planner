21626b1ace92e96be1bbe8d531c81ea2be0a1bed
karanVR <karanveerraghuvanshi@gmail.com>
2024-12-20 10:57:10 +0530

    feat/authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import Image from 'next/image';
 
 export default function Home() {
-  return (
-   <div className='text-3xl font-bold'>Travel Planner</div>
-  );
+  return <div className="text-3xl font-bold">Travel Planner</div>;
 }
