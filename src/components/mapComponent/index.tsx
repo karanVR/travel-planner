b09590d67866ba6f2be1079c2b6241edc83e905f
karanVR <karanveerraghuvanshi@gmail.com>
2024-12-26 12:59:59 +0530

    feat: map loader

--- src/components/mapComponent/index.tsx
+++ src/components/mapComponent/index.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
-
+import dynamic from 'next/dynamic';
+import { memo } from 'react';
+import { Skeleton } from '../ui/skeleton';
+const DynamicLoader = dynamic(() => import('@/components/loader'), {
+  ssr: false,
+});
 interface Place {
   latitude: number;
   longitude: number;
   name: string;
 }
 
+
+const areEqual = (prevProps: MapComponentProps, nextProps: MapComponentProps) => {
+  return JSON.stringify(prevProps.places) === JSON.stringify(nextProps.places);
+};
+
+
 interface MapComponentProps {
   places: Place[];
 }
 
-const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API;
+const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API || '';
 
 const MapComponent: React.FC<MapComponentProps> = ({ places }) => {
   const containerStyle = { width: '100%', height: '400px' };
@@ -21,8 +32,12 @@ const MapComponent: React.FC<MapComponentProps> = ({ places }) => {
     lng: places.length > 0 ? places[0].longitude : 0,
   };
 
+  if (!API_KEY) {
+    return <div><DynamicLoader /></div>;
+  }
+
   return (
-    <LoadScript googleMapsApiKey={API_KEY!}>
+    <LoadScript googleMapsApiKey={API_KEY} loadingElement={ <Skeleton className="h-[61%] w-[100%] rounded-xl" />}>
       <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={14}>
         {places.map((place, index) => (
           <Marker
@@ -36,4 +51,4 @@ const MapComponent: React.FC<MapComponentProps> = ({ places }) => {
   );
 };
 
-export default MapComponent;
+export default memo(MapComponent, areEqual);
